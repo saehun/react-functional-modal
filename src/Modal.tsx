76c3705c782bcefa782a660173848b1d5d9dbbec
minidonut <nycom13@gmail.com>
2020-05-08 21:08:30 +0900

    Change hide api

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -6,56 +6,52 @@ const idPrefix = String(Number(new Date())).slice(7); // prevent id of element f
 /* const isBrowser = typeof window === "object"; */
 
 // initialize global object.
-const [root, instances] = ((): [HTMLElement, Record<string, React.ReactNode>] => {
+const [root, instances] = ((): [HTMLElement, Record<string, { instance: React.ReactNode; el: HTMLElement }>] => {
   const el = document.createElement("div");
-  const instances: Record<string, React.ReactNode> = {};
+  const instances: Record<string, { instance: React.ReactNode; el: HTMLElement }> = {};
   el.setAttribute("id", `modal-root-${idPrefix}`);
   document.body.appendChild(el);
 
   return [el, instances];
 })();
 
-class Instance extends React.Component {
+class Instance extends React.Component<any> {
 
   state: any = {
     children: null,
-    show: false,
   }
 
   show(children: React.ReactNode) {
-    this.setState({ show: true, children });
+    this.setState({ children });
   }
 
   hide() {
-    this.setState({ show: false, children: null });
   }
 
   render() {
-    return this.state.show ? (
-      <div>{this.state.children}</div>
-    ) : null;
+    return <div>{this.state.children}</div>;
   }
 }
 
 
-const getInstance = (callback: any, key?: string) => {
-  console.log(instances, key);
+const getInstance = (callback: any, key: string) => {
+
   if (key in instances) {
-    callback(instances[key]);
+    callback(instances[key].instance);
     return;
   }
 
+  const el = document.createElement("div");
+  root.appendChild(el);
+
   const ref = (instance: any) => {
+    if (!instance) return;
     callback(instance);
-    instances[key] = instance;
+    instances[key] = { instance, el };
     return instance;
   };
 
-  const el = document.createElement("div");
-  el.setAttribute("id", `modal-item-${idPrefix}`);
-  root.appendChild(el);
-
-  ReactDOM.render(<Instance ref={ref} />, el);
+  ReactDOM.render(<Instance ref={ref} el={el} />, el);
 };
 
 interface Option {
@@ -64,13 +60,22 @@ interface Option {
 
 export const show = (children: React.ReactNode, option?: Option) => {
   getInstance((instance: any) => {
-    console.log(instance);
     instance.show(children);
   }, option?.key || String(counter++));
 };
 
 export const hide = (key?: string) => {
-  getInstance((instance: any) => {
-    instance.hide();
-  }, key);
+  const _key = typeof key === "string" ? key : Object.keys(instances).reverse()[0];
+  if (_key === undefined) {
+    return;
+  }
+
+  if (_key in instances) {
+    const { el } = instances[_key];
+    ReactDOM.unmountComponentAtNode(el);
+    root.removeChild(el);
+    delete instances[_key];
+    return;
+  }
+
 };
