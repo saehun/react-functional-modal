5cc04f1d515c566015f1fcf5418421ef84786b02
minidonut <nycom13@gmail.com>
2020-05-09 17:57:08 +0900

    change hide method.

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import styled, { keyframes } from "styled-components";
-
-const fadeIn = keyframes`
-from {
-  opacity: 0;
-} to {
-  opacity: 1.0;
-}
-`;
+import styled, { keyframes, css } from "styled-components";
 
 let counter = 0;
-const idPrefix = String(Number(new Date())).slice(7); // prevent id of element from overlapping
-/* const isBrowser = typeof window === "object"; */
+const instances: InstanceItem[] = [];
+const fadeIn = keyframes`
+   from {
+     opacity: 0;
+   }
+   to {
+     opacity: 1.0;
+   }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1.0;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+
 
 // initialize global object.
 interface InstanceItem {
@@ -24,6 +34,7 @@ interface InstanceItem {
 
 interface Option {
   key?: string;
+  fading?: boolean;
   styles?: {
     justifyContent?: string;
     alignItems?: string;
@@ -33,34 +44,35 @@ interface Option {
   clickOutsideToClose?: boolean;
 }
 
-const [root, instances] = ((): [HTMLElement, InstanceItem[]] => {
-  const el = document.createElement("div");
-  const instances: any = [];
-  el.setAttribute("id", `modal-root-${idPrefix}`);
-  document.body.appendChild(el);
+const fading = css<{ show: boolean }>`
+  animation: ${props => props.show ? fadeIn : fadeOut} 0.2s ease-out;
+  animation-fill-mode: forwards;
+`;
 
-  return [el, instances];
-})();
-
-const Container = styled.div`
+const Container = styled.div<{ show: boolean, fading: boolean }>`
   display: flex;
   position: fixed;
   top: 0; left: 0; right: 0; bottom: 0;
   justify-content: center;
   align-items: center;
   background: rgba(255,255,255,0);
-  animation: ${fadeIn} 0.2s ease-out forward;
+  ${props => props.fading ? fading : ""}
 `;
 
 class Instance extends React.Component {
 
-  state: { children: React.ReactNode; option: Option } = {
+  state: { children: React.ReactNode; option: Option; show: boolean; } = {
     children: null,
     option: {},
+    show: true,
   }
 
   show(children: React.ReactNode, option: Option) {
     this.setState({ children, option });
+  }
+
+  hide() {
+    this.setState({ show: false });
   }
 
   componentWillUnmount() {
@@ -78,7 +90,11 @@ class Instance extends React.Component {
   }
 
   render() {
-    return <Container onClick={this.handleClickOutside} style={this.state.option?.styles}>
+    return <Container
+      show={this.state.show}
+      fading={!!this.state.option.fading}
+      onClick={this.handleClickOutside}
+      style={this.state.option?.styles}>
       {this.state.children}
     </Container>;
   }
@@ -90,6 +106,7 @@ const getInstance = (callback: any, _option?: Option) => {
   const option: Option = {
     key: String(counter++),
     onClose: () => { },
+    fading: true,
     clickOutsideToClose: true,
     ..._option,
   };
@@ -102,7 +119,7 @@ const getInstance = (callback: any, _option?: Option) => {
   }
 
   const el = document.createElement("div");
-  root.appendChild(el);
+  document.body.appendChild(el);
 
   const ref = (instance: any) => {
     if (!instance) return;
@@ -120,21 +137,34 @@ export const show = (children: React.ReactNode, option?: Option) => {
   }, option);
 };
 
+const hideAndRemove = (i: InstanceItem) => {
+  const { el, option } = i;
+  const instance = i.instance as any;
+  if (option.fading) {
+    console.log("fading1");
+    instance.hide();
+    setTimeout(() => {
+      ReactDOM.unmountComponentAtNode(el);
+      document.body.removeChild(el);
+    }, 200);
+  } else {
+    ReactDOM.unmountComponentAtNode(el);
+    document.body.removeChild(el);
+  }
+};
+
 export const hide = (key?: string) => {
   let i;
   if (typeof key === "string") {
     if (i = instances.find(x => x.key === key)) {
-      const { el } = i;
-      ReactDOM.unmountComponentAtNode(el);
-      root.removeChild(el);
+      hideAndRemove(i);
       instances.splice(instances.indexOf(i), 1);
       return;
     }
   } else {
     if (i = instances.pop()) {
       const { el } = i;
-      ReactDOM.unmountComponentAtNode(el);
-      root.removeChild(el);
+      hideAndRemove(i);
     }
   }
 };
